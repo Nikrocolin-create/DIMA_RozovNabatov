8d3d2dd4cff971b45d291859fc9d46f3ec622c2d
Sergey Nabatov <serynabatov@gmail.com>
2021-01-13 10:57:29 +0300

    delete it

--- dima_nabatov_rozov/android/app/src/main/kotlin/com/example/dima_nabatov_rozov/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dima_nabatov_rozov
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- dima_nabatov_rozov/lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF80DEEa);
-const kPrimaryLightColor = Color(0xFFE0F7FA);
